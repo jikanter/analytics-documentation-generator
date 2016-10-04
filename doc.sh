b71b0f8e2b65648528d7d66407b0285d8edc90f9
jikanter <jikanter@gmail.com>
2016-10-04 14:20:07 -0500

    add package.json, add dependencies, made doc.sh package relative

--- doc.sh
+++ doc.sh
@@ -4,7 +4,7 @@ if [ "${1}" = "" ]; then
   echo "usage: doc.sh <action>"; exit 1;
 fi
 if [ "${1}" = "run" ]; then
- ../../node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
+  node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
 elif [ "${1}" = "publish" ]; then
   cp -r out/ ${PUBLISH_TO}
   cp DataLayer-Map.js ${PUBLISH_TO}
