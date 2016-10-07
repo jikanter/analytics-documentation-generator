9b0e5e29aea7155d33c19e9d19d7213a2a42125f
Jordan Kanter <jikanter@gmail.com>
2016-10-07 17:00:55 +0000

    Update Business-Requirements-Source.js

---
+++ Business-Requirements-Source.js
@@ -0,0 +1,20 @@
+/**
+ * @file DataLayer-Map.js
+ * @author Jordan Kanter <jordan.kanter@stratigent.com>
+ * @fileoverview Documents the United DataLayer
+ * The Data Object Map for United. Includes both {@link Bootstrapper.dataObject} and {@link UA}
+ * Notes:
+ * I have included documentation for {@link Bootstrapper.dataObject} and {@link UA} in 3 forms
+ * <ol>
+ * <li>Browsable, via <a href="http://usejsdoc.org/">jsdoc</a></li>
+ * <li>Code, in javascript</li>
+ * <li>Prose, in the comments</li>
+ * </ol>
+ *
+ * You can follow along using the generated html from the <code>jsdoc</code>, the inline code, or by reading the comments/code.
+ * You can follow along using the generated html from the <code>jsdoc</code>, the inline code, or by reading the comments/code.
+ * You can also generate the contents yourself with jsdoc by installing using npm and running:
+ * <pre>npm install jsdoc</pre>
+ * Please note that this is not the actual data layer, but documentation about the data layer
+ *
+ */
