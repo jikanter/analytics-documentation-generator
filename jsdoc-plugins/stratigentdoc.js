8aa888ced3166ed0c46f222ff335f2781d0ef7ec
Jordan Kanter <jikanter@gmail.com>
2016-10-10 17:22:28 -0500

    added the beginnings of custom output filters for stratigent docs

---
+++ jsdoc-plugins/stratigentdoc.js
@@ -0,0 +1,14 @@
+exports.defineTags = function _defineTags(dictionary) {
+  // defining stratigent tags
+  dictionary.defineTag('dl', {
+    onTagged: function _onTagged(doclet, tag) {}
+  });
+
+  dictionary.defineTag('useraction', {
+    onTagged: function _onTagged(doclet, tag) {}
+  });
+
+  dictionary.defineTag('populate', {
+    onTagged: function _onTagged(doclet, tag) {}
+  });
+};
