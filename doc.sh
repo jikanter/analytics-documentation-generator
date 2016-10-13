839a41f88121b89318dac460d53014de234e60ce
Jordan Kanter <jikanter@gmail.com>
2016-10-13 14:55:05 -0500

    customize the doc.sh --run-script for the individual docs

--- doc.sh
+++ doc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PUBLISH_TO=/c/Users/jordan.kanter/Dropbox\ \(Stratigent\)/Individual\ -\ Jordan/Clients/United/
+#PUBLISH_TO=/c/Users/jordan.kanter/Dropbox\ \(Stratigent\)/Individual\ -\ Jordan/Clients/United/
+PUBLISH_TO=/Users/admin/Dropbox\ \(Stratigent\)/Individual\ -\ Jordan/Clients/United/
 # remove 64 from MINGW
 PLATFORM=$(exec uname -s | cut -d "6" -f 1)
 REMOTE_HOST=
@@ -8,27 +9,26 @@ if [[ "${1}" = "" ]]; then
   echo "usage: doc.sh <action>"; exit 1;
 fi
 
-
 if [ "${1}" = "run" ] && [ "${2}" = "" ]; then
   node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
 elif [ "${1}" = "run" ] && [ "${2}" = "sdr" ]; then
   echo "generating an sdr"
   node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
   if [ "${PLATFORM}" = "MINGW" ]; then
-    /c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" out/index.html out/sdr.pdf
+    /c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent SDR';})()" out/index.html out/sdr.pdf
   else
-    wkhtmltopdf --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" out/index.html out/sdr.pdf
+    wkhtmltopdf --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent SDR';})()" out/index.html out/sdr.pdf
   fi
 elif [ "${1}" = "run" ] && [ "${2}" = "tag-map" ]; then
    node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
   if [ "${PLATFORM}" = "MINGW" ]; then
-  /c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" out/index.html out/tag-map.pdf
+  /c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent Tag Map';})()" out/index.html out/tag-map.pdf
   else
-    wkhtmltopdf --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" out/index.html out/tag-map.pdf
+    wkhtmltopdf --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent Tag Map';})()" out/index.html out/tag-map.pdf
   fi
 elif [ "${1}" = "publish" ]; then
-  cp -r out/ ${PUBLISH_TO}
-  cp DataLayer-Map.js ${PUBLISH_TO}
+  cp -r out/ "${PUBLISH_TO}"
+  cp DataLayer-Map.js "${PUBLISH_TO}"
 elif [ "${1}" = "run-csv" ]; then
   # just experimental. Do not use
   node ./js2csv.js
