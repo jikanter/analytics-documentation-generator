ca1eec2e8eee3e86e45aac624e90bf2fa8a6fe37
Jordan Kanter <jikanter@gmail.com>
2025-12-15 13:42:02 -0600

    update packages

--- doc.sh
+++ doc.sh
@@ -4,31 +4,34 @@
 PUBLISH_TO=$(pwd)/../../doc
 # remove 64 from MINGW
 PLATFORM=$(exec uname -s | cut -d "6" -f 1)
+MKPDF="server/pdfgen"
+WINMKPDF="${MKPDF}"
 REMOTE_HOST=
 FORMATTING_SCRIPT="javascript:(function(){ document.querySelector('h1').setAttribute('style', 'visibility:hidden'); document.querySelector('h2').setAttribute('style', 'visibility:hidden'); document.querySelector('nav h2').setAttribute('style', 'visibility:hidden');})()"
+
 if [[ "${1}" = "" ]]; then
   echo "usage: doc.sh <action>"; exit 1;
 fi
 
-if [ "${1}" = "run" ] && [ "${2}" = "" ]; then
+if [[ "${1}" = "run" ]] && [[ "${2}" = "" ]]; then
   node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
-elif [ "${1}" = "run" ] && [ "${2}" = "server" ]; then
+elif [[ "${1}" = "run" ]] && [[ "${2}" = "server" ]]; then
   node server/ui.js
   node server/static.js
-elif [ "${1}" = "run" ] && [ "${2}" = "sdr" ]; then
+elif [[ "${1}" = "run" ]] && [[ "${2}" = "sdr" ]]; then
   echo "generating an sdr"
   node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
   if [ "${PLATFORM}" = "MINGW" ]; then
-    /c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent SDR';})()" out/index.html out/sdr.pdf
+    ${WINMKPDF} --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent SDR';})()" out/index.html out/sdr.pdf
   else
-    wkhtmltopdf --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent SDR';})()" out/index.html out/sdr.pdf
+    ${MKPDF} --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent SDR';})()" out/index.html out/sdr.pdf
   fi
 elif [ "${1}" = "run" ] && [ "${2}" = "tag-map" ]; then
    node_modules/jsdoc/jsdoc.js --verbose DataLayer-Map.js
   if [ "${PLATFORM}" = "MINGW" ]; then
-  /c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent Tag Map';})()" out/index.html out/tag-map.pdf
+   ${WINMKPDF} --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent Tag Map';})()" out/index.html out/tag-map.pdf
   else
-    wkhtmltopdf --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent Tag Map';})()" out/index.html out/tag-map.pdf
+    ${MKPDF} --javascript-delay 500 --debug-javascript --run-script "${FORMATTING_SCRIPT}" --run-script "javascript:(function(){ document.querySelectorAll('h3')[1].innerText = 'Stratigent Tag Map';})()" out/index.html out/tag-map.pdf
   fi
 elif [ "${1}" = "publish" ]; then
   cp -r out/ "${PUBLISH_TO}"
